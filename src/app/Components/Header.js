4a85cf14eae7f2a3cb744b2a627429a2cde3909a
Hubert Łepski <hubertlepsky@gmail.com>
2024-08-23 17:48:21 +0200

    Redesign newsletter modal and change fonts in header

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -5,6 +5,7 @@ import { FaFacebookF, FaTiktok, FaPinterest, FaTwitter } from 'react-icons/fa';
 import { GrInstagram } from 'react-icons/gr';
 import { MdOutlineClose } from 'react-icons/md';
 import Navbar from './Navbar';
+import Image from 'next/image';
 
 function Header() {
     const modalOpenStateFromSessionStorage = useSelector(
@@ -77,49 +78,74 @@ function Header() {
                         <MdOutlineClose />
                     </p>
 
-                    <div>
+                    <div className='modal_image-wrapper'>
+                        <Image
+                            src='/images/blackHoodie2.jpg'
+                            height={475}
+                            width={475}
+                            alt='product thumbnail'
+                        />
+                        <Image
+                            src='/instaGallery6.jpg'
+                            height={475}
+                            width={475}
+                            alt='product thumbnail'
+                        />
+                    </div>
+
+                    <div className='modal_info-wrapper'>
                         <h2 className='modal_heading'>Join our mailing list</h2>
                         <p className='modal_description'>
                             Sign up for exclusive updates, new arrivals &
                             insider-only discounts and get <span>15%</span> off
                             your first order!
                         </p>
-                    </div>
-
-                    <form onSubmit={handleSubmitModal}>
-                        <label>
-                            <input
-                                id='modalInput'
-                                type='email'
-                                placeholder='Enter your email address'
-                            />
-                            <input
-                                className='modalButton'
-                                type='submit'
-                                value='SUBMIT'
-                            />
-                        </label>
-                    </form>
-
-                    <div className='modal__icons'>
-                        <i>
-                            <GrInstagram size={22.5} />
-                        </i>
-                        <i>
-                            <FaTwitter size={22.5} />
-                        </i>
-                        <i>
-                            <FaFacebookF size={22.5} />
-                        </i>
-                        <i>
-                            <FaPinterest size={22.5} />
-                        </i>
-                        <i>
-                            <FaTiktok size={22.5} />
-                        </i>
+                        <form onSubmit={handleSubmitModal}>
+                            <label>
+                                <input
+                                    id='modalInput'
+                                    type='email'
+                                    placeholder='Enter your email address'
+                                />
+                                <input
+                                    className='modalButton'
+                                    type='submit'
+                                    value='SUBMIT'
+                                />
+                                <p className='modal_rules'>
+                                    *By submitting this form, you consent to
+                                    receive our Newsletter and agree to our
+                                    <span> Privacy Policy.</span>
+                                </p>
+                            </label>
+                        </form>
+                        <div className='modal__icons'>
+                            <i>
+                                <GrInstagram size={22.5} />
+                            </i>
+                            <i>
+                                <FaTwitter size={22.5} />
+                            </i>
+                            <i>
+                                <FaFacebookF size={22.5} />
+                            </i>
+                            <i>
+                                <FaTiktok size={22.5} />
+                            </i>
+                        </div>
                     </div>
                 </div>
             )}
+
+            {/*    <div className='bottom_bar'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Possimus eligendi minus tempore, rerum itaque ex ipsam cum
+                    accusamus eos odit sapiente impedit, maiores repudiandae
+                    quos asperiores nihil.
+                </p>
+                <MdOutlineClose className='icon' />
+            </div>*/}
         </header>
     );
 }
